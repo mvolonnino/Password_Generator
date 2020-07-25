2cb8e97d6771e3cc483cbc3b2d4219f1711cbea3
Matt Volonnino <mvolonnino12@gmail.com>
2020-07-25 13:10:41 -0400

    removed and added console.log to and added comments to further understand make it the code easier to read

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code -
 var generateBtn = document.querySelector("#generate");
 var lettersLower = "abcdefghijklmnopqrstuvwxyz".split("");
 var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -6,7 +6,7 @@ var numbers = "123456789".split("");
 var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
 console.log(
-  "This is to show the arrays made for each character selection that user will be confirmed to have in their password: ",
+  "This is all the character sets in taken from a string and made into an array using the .split(): ",
   lettersLower,
   lettersUpper,
   numbers,
@@ -19,27 +19,27 @@ function getPasswordOptions() {
     "How long would you like your password to be? Must between 8 and 128 characters long."
   );
 
-  if (userLengthPw < 8 || userLengthPw > 128) {
+  while (userLengthPw < 8 || userLengthPw > 128 || userLengthPw === null) {
     userLengthPw = prompt(
       "Do you know how to read? I said between 8 and 128 you fool!"
     );
-  }
+  } 
 
   var userLettersLower = confirm(
-    "Would you like lowercase letters in your password? (cancel for no, ok for yes)"
-  );
+      "Would you like lowercase letters in your password? (cancel for no, ok for yes)"
+    );
 
   var userLettersUpper = confirm(
-    "Would you like UPPERCASE letters in your password? (cancel for no, ok for yes)"
-  );
+      "Would you like UPPERCASE letters in your password? (cancel for no, ok for yes)"
+    );
 
   var userNumbers = confirm(
-    "Would you like numbers in your password? (cancel for no, ok for yes)"
-  );
+      "Would you like numbers in your password? (cancel for no, ok for yes)"
+    );
 
-  var userSymbols = confirm(
-    "Would you like special characters in your password? (cancel for no, ok for yes)"
-  );
+  var userSymbols = confirm("Would you like special characters in your password? (cancel for no, ok for yes)"
+    );
+  }
 
   var passwordOptions = {
     userLengthPw: userLengthPw,
@@ -50,7 +50,7 @@ function getPasswordOptions() {
   };
 
   console.log(
-    "These are the user requested password options: ",
+    "Object that stores user responses for password: ",
     passwordOptions
   );
 
@@ -59,44 +59,40 @@ function getPasswordOptions() {
 
 function generatePassword() {
   var userOptions = getPasswordOptions(); //taking passwordOptions as an object that has the user inputs stored
-  console.log("userOptions: ", userOptions);
+
   var possiblePassword = []; //stores the array types of characters to include the final password
 
+  //these ifs take the object userOptions key values, if they are TRUE then it will merge the arrays into the possiblePassword[]
   if (userOptions.userLettersLower) {
     possiblePassword = possiblePassword.concat(lettersLower);
-    console.log("lettersLower: ", lettersLower);
   }
   if (userOptions.userLettersUpper) {
     possiblePassword = possiblePassword.concat(lettersUpper);
-    console.log("lettersUpper: ", lettersUpper);
   }
   if (userOptions.userNumbers) {
     possiblePassword = possiblePassword.concat(numbers);
-    console.log("numbers: ", numbers);
   }
   if (userOptions.userSymbols) {
     possiblePassword = possiblePassword.concat(symbols);
-    console.log("symbols: ", symbols);
   }
-  console.log(possiblePassword);
-
+  console.log(
+    "This will be an array of all the possible characters in the password: ",
+    possiblePassword
+  );
   return getRandom(userOptions.userLengthPw, possiblePassword);
 }
 
 // getRandom function
 function getRandom(userLength, possiblePassword) {
-  console.log("userLength of password: ", userLength);
-  console.log("possiblePassword: ", possiblePassword);
   var strPossiblePassword = possiblePassword.join("");
-  console.log("string possiblePassword ", strPossiblePassword);
+  console.log("String for the possiblePassword[] ", strPossiblePassword);
   var randomPassword = "";
   for (var i = 0; i < userLength; i++) {
     randomPassword += strPossiblePassword.charAt(
       Math.floor(Math.random() * strPossiblePassword.length)
     );
   }
-  console.log("This is the random characters: ", randomPassword);
-  console.log("randomPassword.length: ", randomPassword.length);
+  console.log("The length for the possiblePassword: ", randomPassword.length);
   return randomPassword;
 }
 
